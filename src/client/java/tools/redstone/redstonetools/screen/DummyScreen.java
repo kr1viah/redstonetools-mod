7df805a979b5e330bf0bbf6fc3110f1ac36932e4
kr1viah <kravpolet@gmail.com>
2025-10-06 16:48:36 +0200

    DummyScreen now interacts with chat

--- src/client/java/tools/redstone/redstonetools/screen/DummyScreen.java
+++ src/client/java/tools/redstone/redstonetools/screen/DummyScreen.java
@@ -2,32 +2,35 @@ package tools.redstone.redstonetools.screen;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.Text;
+import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.text.Style;
 
-public class DummyScreen extends Screen {
+public class DummyScreen extends ChatScreen {
 	public DummyScreen() {
-		super(Text.of(""));
+		super("");
 		client = MinecraftClient.getInstance();
 	}
 
 	@Override
-	public void renderBackground(DrawContext context, int mouseX, int mouseY, float deltaTicks) {
-	}
-
-	@Override
 	public void render(DrawContext context, int mouseX, int mouseY, float deltaTicks) {
+		this.client.inGameHud.getChatHud().render(context, this.client.inGameHud.getTicks(), mouseX, mouseY, true);
 	}
 
 	@Override
 	public boolean mouseClicked(double mouseX, double mouseY, int button) {
-		assert client != null;
-		client.setScreen(null);
-		return true;
-	}
+		if (button == 0) {
+			ChatHud chatHud = this.client.inGameHud.getChatHud();
+			if (chatHud.mouseClicked(mouseX, mouseY)) {
+				return true;
+			}
 
-	@Override
-	public boolean shouldPause() {
-		return false;
+			Style style = this.client.inGameHud.getChatHud().getTextStyleAt(mouseX, mouseY);
+			if (style != null && this.handleTextClick(style)) {
+				return true;
+			}
+		}
+		this.client.setScreen(null);
+		return true;
 	}
 }
